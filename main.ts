3fb641e9373f08d90e2ce91049610e36e3929bf6
Ho Yin <88828097+lhy-hoyin@users.noreply.github.com>
2024-11-26 06:07:40 +0000

    Quest 2 - Step 6

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
-import { hello } from "https://gist.githubusercontent.com/uncomfyhalomacro/b5de275dfcbaeff1d2d89739541e4e6f/raw/648e323e4ab872cc2a6eeafdd1ccf46d8f6c6301/mod.ts";
+import { mutiplyWords } from "multiply-words";
 
 if (import.meta.main) {
-  hello();
+  const multipliedWords = mutiplyWords(Deno.args[0].trim(), Number.parseInt(Deno.args[1].trim()));
+  console.log(multipliedWords)
 }
