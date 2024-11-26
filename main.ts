e340727fc2cf6148aa0b545b5da4b8aaa29bfad8
Ho Yin <88828097+lhy-hoyin@users.noreply.github.com>
2024-11-26 06:26:52 +0000

    Quest 2 - Step 10

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
-function foo(s: any): any {
-  return "foo"
-}
-
 if (import.meta.main) {
-  let foobar = "bar";
-  const barsoap = 0;
-  console.log(foo("foo"), foobar);
+  Deno.serve((_req_) => {
+    (async () => {
+      for await (const el of Deno.readDir(".")) {
+        console.log(el.name);
+      }
+    })();
+    const hello = new Response("Hello, Stackie!");
+    return hello;
+  });
 }
