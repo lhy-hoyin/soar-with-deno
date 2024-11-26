ec08157584b25382b490344d67335c22cb7e998e
Ho Yin <88828097+lhy-hoyin@users.noreply.github.com>
2024-11-26 06:00:20 +0000

    Quest 2 - Step 4

--- main.ts
+++ main.ts
@@ -1,6 +1,25 @@
-import { mutiplyWords } from "./multiply-words.ts";
+/*
+import { Application, Router } from "jsr:@oak/oak@17.1.3";
+const router = new Router();
+const app = new Application();
+
+router.get("/", (ctx) => {
+    ctx.response.body = "Hello, World!\n";
+})
+
+app.use(router.allowedMethods());
+app.use(router.routes());
+
+/*/
+import express from "npm:express@4.21.1";
+const app = express();
+
+app.get("/", (_req, res) => {
+        res.send("Hello, World!\n");
+});
+/**/
+
 
 if (import.meta.main) {
-  const multipliedWords = mutiplyWords(Deno.args[0].trim(), Number.parseInt(Deno.args[1].trim()));
-  console.log(multipliedWords)
+    app.listen({ port: 8080 });
 }
